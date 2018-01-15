ca4367bfbdc456b5e7837070896a6098155c2943
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-01-15 11:09:39 +0100

    first commit

---
+++ src/main/java/io/quantixx/book/client/isbn/ClientConfiguration.java
@@ -0,0 +1,12 @@
+package io.quantixx.book.client.isbn;
+
+import io.quantixx.book.client.ExcludeFromComponentScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ExcludeFromComponentScan
+@EnableConfigurationProperties
+public class ClientConfiguration {
+
+}
